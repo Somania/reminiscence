b7e24436ebd9225675626f85c20b03ef263c0430
somania <somanianurmahal@gmail.com>
2018-04-05 17:49:13 +0600

    controller action addedk

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,39 @@
 class PostsController < ApplicationController
 
   def index
-      @posts=Post.all
+    @posts = Post.all
   end
 
   def show
-    @post=Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
+
   def new
-     @post=Post.new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
   end
 
   def create
-     @post=Post.new(post_params)
-     if @post.save
-       redirect_to@post
-     else
-       render 'new'
-     end
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
-      def post_params
-        params.require(:post).permit(:title, :image)
-      end
 
-
+  def post_params
+    params.require(:post).permit(:title, :image)
+  end
 end
